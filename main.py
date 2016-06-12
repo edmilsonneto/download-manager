292136c4f5a3954f1ee019669b32fd51b6d9b8a0
Edmilson Neto <edmilsonneto10@gmail.com>
2016-06-12 13:28:53 -0300

    last commit

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import util
 
 
 def main():
+    # recupera o comunicador
     comm = MPI.COMM_WORLD
 
     # obtem o ranque do processo atual
     rank = comm.Get_rank()
 
     # define a url do arquivo a ser baixado
-    url = 'https://alphard.sscdn.co/palcomp3/2/f/3/d/westhillband-one-more-night-in-heaven-9d2282.mp3'
+    url = 'http://api.soundcloud.com/tracks/86979624/stream?client_id=36c93f8b49b8a30bd3bab88e57e4758a&.mp3'
 
     # processo coordenador
     if rank == 0:
@@ -19,16 +20,16 @@ def main():
         intervalos = util.fabricar_intervalos(url)
 
         # manda cada intervalo para um processo
-        comm.send(intervalos[0], dest=1, tag=11)
-        comm.send(intervalos[1], dest=2, tag=11)
-        comm.send(intervalos[2], dest=3, tag=11)
-        comm.send(intervalos[3], dest=4, tag=11)
+        comm.send(intervalos[0], dest=1)
+        comm.send(intervalos[1], dest=2)
+        comm.send(intervalos[2], dest=3)
+        comm.send(intervalos[3], dest=4)
 
         # verifica se todos os processos baixaram suas partes com sucesso
-        if comm.recv(source=1, tag=11)\
-                and comm.recv(source=2, tag=11)\
-                and comm.recv(source=3, tag=11)\
-                and comm.recv(source=4, tag=11):
+        if comm.recv(source=1)\
+                and comm.recv(source=2)\
+                and comm.recv(source=3)\
+                and comm.recv(source=4):
 
                 # junta os arquivos temporarios em um unico arquivo
             util.juntar_arquivos_temporarios(url)
@@ -38,29 +39,29 @@ def main():
 
     # processos operarios
     if rank == 1:
-        intervalo = comm.recv(source=0, tag=11)
+        intervalo = comm.recv(source=0)
 
         baixouComSucesso = util.baixar_uma_parte_do_arquivo(url, 0, intervalo)
 
-        comm.send(baixouComSucesso, dest=0, tag=11)
+        comm.send(baixouComSucesso, dest=0)
     if rank == 2:
-        intervalo = comm.recv(source=0, tag=11)
+        intervalo = comm.recv(source=0)
 
         baixouComSucesso = util.baixar_uma_parte_do_arquivo(url, 1, intervalo)
 
-        comm.send(baixouComSucesso, dest=0, tag=11)
+        comm.send(baixouComSucesso, dest=0)
     if rank == 3:
-        intervalo = comm.recv(source=0, tag=11)
+        intervalo = comm.recv(source=0)
 
         baixouComSucesso = util.baixar_uma_parte_do_arquivo(url, 2, intervalo)
 
-        comm.send(baixouComSucesso, dest=0, tag=11)
+        comm.send(baixouComSucesso, dest=0)
     if rank == 4:
-        intervalo = comm.recv(source=0, tag=11)
+        intervalo = comm.recv(source=0)
 
         baixouComSucesso = util.baixar_uma_parte_do_arquivo(url, 3, intervalo)
 
-        comm.send(baixouComSucesso, dest=0, tag=11)
+        comm.send(baixouComSucesso, dest=0)
 
 
 if __name__ == '__main__':
